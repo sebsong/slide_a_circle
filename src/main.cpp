9bad37a5164f462f5ca6a0aa8cd25165063b1f11
Sebastian Song <sebsong@berkeley.edu>
2020-12-30 17:30:46 -0800

    move normalized vector logic to sebgine

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,11 @@
 
 const std::string GAME_NAME{"slide_a_circle"};
 
-template <class T>
-static sf::Vector2<T> getNormalizedVector(const sf::Vector2<T> vector)
-{
-    float magnitude{seb::getVectorMagnitude(vector)};
-    if (magnitude == 0)
-    {
-        return vector;
-    }
-
-    return vector / magnitude;
-}
-
 static void moveTowardPoint(sf::Transformable& toMove, const sf::Vector2f& point, const float& speed)
 {
     sf::Vector2f currPos{toMove.getPosition()};
     sf::Vector2f dir{point - currPos};
-    sf::Vector2f movementVector{getNormalizedVector(dir) * speed};
+    sf::Vector2f movementVector{seb::getNormalizedVector(dir) * speed};
 
     if (seb::getVectorMagnitude(movementVector) > seb::getVectorMagnitude(dir))
     {
